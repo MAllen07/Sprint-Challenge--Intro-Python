9eca4f524f41dfa56433112cdfe5ec72127a287d
Mindy Allen <mrsmallen07@gmail.com>
2019-09-06 14:38:21 -0400

    created class hierarchy for eacb vehicle

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,6 +1,6 @@
 # Write classes for the following class hierarchy:
 #
-#  [Vehicle]->[FlightVehicle]->[Starship]
+#  [Vehicle]->[FlightVehicle]->[Starship]  #base
 #      |                |
 #      v                v
 # [GroundVehicle]      [Airplane]
@@ -16,34 +16,87 @@
 # Put a comment noting which class is the base class
 
 
-class Vehicle:    #this is the base class
-	def __init__(self, name): 
-       self.name = name 
+# class Vehicle:    #this is the base class
+# 	def __init__(self, name): 
+#        self.name = name 
+#     pass
+
+
+
+class Vehicle:
     pass
-    
+
+
 class GroundVehicle(Vehicle):
-	def __init__(self, name):
-		self.name = name 
     pass
 
- class Car(GroundVehicle):
- 	def __init__(self, name):
-		self.name = name 
-    pass
- class Motorcycle(GroundVehicle):
- 	def __init__(self, name):
-		self.name = name
+
+class Car(GroundVehicle):
     pass
 
- class FlightVehicle(Vehicle):
- 	def __init__(self, name):
-		self.name = name
+
+class Motorcycle(GroundVehicle):
     pass
- class Airplane(FlightVehicle):
- 	def __init__(self, name):
-		self.name = name
+
+
+class FlightVehicle(Vehicle):
     pass
- class Starship(FlightVehicle):
- 	def __init__(self, name):
-		self.name = name
+
+
+class Airplane(FlightVehicle):
     pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+# class Vehicle:
+#     def __init__(self, name, type):
+#         self.name = name
+#         self.type = type
+
+
+# class GroundVehicle(Vehicle):
+#     def __init__(self, name):
+#         Vehicle.__init__(self, name, "Ground Vehicle")
+
+
+# class Car(GroundVehicle):
+#     def __init__(self, name, model):
+#         self.model = model
+#         self.wheels = 4
+#         GroundVehicle.__init__(self, name)
+
+#     def __str__(self):
+#         print(
+#             f"Car name: {self.name} is model type: {self.model} of vehichle type: {self.type}.")
+
+
+# class Motorcycle(GroundVehicle):
+#     def __init__(self, name, model):
+#         self.model = model
+#         self.wheels = 2
+#         GroundVehicle.__init__(self, name)
+
+#     def __str__(self):
+#         print(
+#             f"Motorcycle name: {self.name} is model type: {self.model} of vehichle type: {self.type}, that has {self.wheels} number of wheels.")
+
+
+# class FlightVehicle:
+#     def __init__(self, name):
+#         self.name = name
+#         self.type = "Flight Vehicle"
+
+
+# class Airplane(FlightVehicle):
+#     def __init__(self, name):
+#         self.model = "Airplane"
+#         FlightVehicle.__init__(self, name)
+
+
+# class Starship:
+#     def __init__(self, name):
+#         self.name = name
+#         self.type = "Star Traveller"
