9679de264f87aff8045f2c12098af7564ce76208
Mindy Allen <mrsmallen07@gmail.com>
2019-09-06 14:39:31 -0400

    added a method called drive that returned vroom

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,32 +1,40 @@
-# To the GroundVehicle class, add method drive() that returns "vroooom".
-#
-# Also change it so the num_wheels defaults to 4 if not specified when the
-# object is constructed.
+# # To the GroundVehicle class, add method drive() that returns "vroooom".
+# #
+# # Also change it so the num_wheels defaults to 4 if not specified when the
+# # object is constructed.
 
-class GroundVehicle():
-    def __init__(self, num_wheels):
-        self.num_wheels = num_wheels
+# class GroundVehicle():
+#     def __init__(self, num_wheels=4): #defualts to 4
+#         self.num_wheels = num_wheels
 
-    # TODO
+#     def drive(self):
+#         return "vroooom"
 
 
-# Subclass Motorcycle from GroundVehicle.
-#
-# Make it so when you instantiate a Motorcycle, it automatically sets the number
-# of wheels to 2 by passing that to the constructor of its superclass.
-#
-# Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+# # Subclass Motorcycle from GroundVehicle.
+# #
+# # Make it so when you instantiate a Motorcycle, it automatically sets the number
+# # of wheels to 2 by passing that to the constructor of its superclass.
+# #
+# # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+# class Motorcycle(GroundVehicle):
+#     def __init__(self):
+#         super(GroundVehicle).__init__(num_wheels=2)
 
-vehicles = [
-    GroundVehicle(),
-    GroundVehicle(),
-    Motorcycle(),
-    GroundVehicle(),
-    Motorcycle(),
-]
+#     def drive(self):
+#         return "BRAAAP!!"
 
-# Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+# vehicles = [
+#     GroundVehicle(),
+#     GroundVehicle(),
+#     Motorcycle(),
+#     GroundVehicle(),
+#     Motorcycle(),
+# ]
+
+# # Go through the vehicles list and print the result of calling drive() on each.
+
+# for vehicle in vehicles:
+#     print(vehicle.drive())
